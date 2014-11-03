f8aa4caf37775800945b5d6ff69bb56cab2898f5
Dominik Halfkann <Dominik.Halfkann@googlemail.com>
2014-11-03 19:23:34 +0100

    Hauptmenue hinzugefügt, Zeitleiste hinzugefügt

--- Assets/Scripts/LifeUIBehavior.cs
+++ Assets/Scripts/LifeUIBehavior.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class LifeUIBehavior : MonoBehaviour {
 
     public int lifecount = 1;
+    public float hSliderValue = 0.0F;
 
 	// Use this for initialization
 	void Start () {
@@ -11,10 +12,11 @@ public class LifeUIBehavior : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+    void Update()
+    {
         if (lifecount > Globals.Lifes)
         {
             Destroy(this.gameObject);
         }
-	}
+    }
 }
---
+++ Assets/Scripts/TimeSlider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeSlider : MonoBehaviour {
+
+    public float maxTime = 100F;
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float sliderPos = Time.timeSinceLevelLoad / maxTime;
+        this.GetComponent<Slider>().value = sliderPos;
+
+        Debug.Log(Time.timeSinceLevelLoad.ToString());
+        if (Time.timeSinceLevelLoad.ToString() == "100")
+        {
+            Application.LoadLevel("MainMenu");
+        }
+    }
+}
